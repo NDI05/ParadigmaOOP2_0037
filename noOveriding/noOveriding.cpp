984ef32388b4883609b333c126bad32f540bd1b8
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2025-06-02 10:43:03 +0700

    create class base class

--- noOveriding/noOveriding.cpp
+++ noOveriding/noOveriding.cpp
@@ -1,3 +1,9 @@
 #include <iostream>
 using namespace std;
 
+class baseClass{
+    public:
+        virtual void perkenalan(){
+            cout << "Halo Saya Function dari base class";
+        }
+};
