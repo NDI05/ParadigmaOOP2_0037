46a554cd6da56df1a8c7aeb73ca3671a854c8fa7
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2025-06-02 10:34:50 +0700

    create class budi

--- multiwaris/multiwaris.cpp
+++ multiwaris/multiwaris.cpp
@@ -9,16 +9,24 @@ class orang{
         }
 };
 
-class pekerja : virtual public orang{
+class pekerja:virtual public orang{
     public:
         pekerja(int pUmur):orang(pUmur){
             cout << "pekerja dibuat\n" << endl;
         }
 };
 
-class pelajar : virtual public orang{
+class pelajar:virtual public orang{
     public:
         pelajar(int pUmur):orang(pUmur){
             cout << "pelajar dibuat\n" << endl;
         }
 };
+
+class budi:public pekerja, public pelajar{
+    public:
+        budi(int pUmur):pekerja(pUmur), pelajar(pUmur), orang(pUmur){
+            cout << "Budi Di Buat\n" << endl;
+        }
+};
+
