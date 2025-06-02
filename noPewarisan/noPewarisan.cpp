74f74ea69947cca8aa161ba32f80fb46c3d9fb4b
Andhika Pratama <p21-andhika355@smkn7-smr.sch.id>
2025-06-02 10:51:04 +0700

    create new class

--- noPewarisan/noPewarisan.cpp
+++ noPewarisan/noPewarisan.cpp
@@ -1,3 +1,10 @@
 #include <iostream>
 using namespace std;
 
+class baseClass{
+    public:
+        virtual void perkenalan(){
+            cout << "Halo Saya Function dari base class";
+        }
+};
+
